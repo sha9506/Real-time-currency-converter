38758979d3ab4ddc2061e2a87460022dbd1c9303
sabnam <sabnamhazari@gmail.com>
2024-05-26 22:55:04 +0530

    completed

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,76 +1,100 @@
 import React, { useEffect, useState } from "react";
+import Converter from '../Assets/converter.png';
 
 const Landing=()=>
 {
     const[country, setCountry] = useState({});
+    const[countryValue, setCountryValue] = useState(0);
+    const[fromCurrency, setFromCurrency] = useState('');
+    const[fromCountryName, setFromCountryName]=useState('');
+    const[toCurrency, setToCurrency]= useState('');
+    const[toCountryName, setToCountryName]=useState('');
+    const[inputValue, setInputValue]=useState(0);
+    
+
+
+    const fetchData = async()=>{
+        const code = await fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json');
+        const jsonCode = await code.json();
+        setCountry(jsonCode);
+
+    }
 
     useEffect(() =>{
-        const fetchData = async()=>{
-            const result = await fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json');
-            const jsonResult = await result.json();
-            console.log(jsonResult);
-            setCountry(jsonResult);
-        }
-
         fetchData();
     },[])
 
+    const handleConvert= async()=>
+        {
+         
+            const number = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurrency}.json`);
+            const jsonNumber = await number.json();
+            setCountryValue(jsonNumber[fromCurrency][toCurrency]);
+            console.log(jsonNumber[fromCurrency][toCurrency])
+        
+        }
 
 
     return(<div className="landing-box">
         <div className="heading">
-            1,223 USD to CAD- Convert US Dollars to Canadian Dollars
+            {inputValue} {fromCurrency} to {toCurrency}- Convert {fromCountryName} to {toCountryName}
         </div>
         <div className="main-box">
            <div className="convert-text"> Convert</div>
            <div className="input-container">
             <div className="flex-1">
                 <div className="input-heading">Amount</div>
-                <input type="number" className="input-full" />
+                <input type="number" onChange={event=>{setInputValue(event.target.value)}} className="input-full" />
             </div>
             <div className="flex-1">
                 <div className="input-heading">From</div>
-                <select name="" id="" className="input-full">
-                    <option value="India"> India</option>
-                    <option value="India"> India</option>
-                    <option value="India"> India</option>
+                <select value={fromCurrency} onChange={event=>{setFromCurrency(event.target.value);
+                    setFromCountryName(country[event.target.value])}} className="input-full">
+                {
+                        Object.keys(country).map((key)=>
+                        (
+                            country[key]? <option value={key} >{country[key]}</option> : null 
+                        ))
+                    }
                 </select>
             </div>
             <div className="circle">
-                ---
+                <img src={Converter} alt="converter" onClick={()=>{
+                    let temp = fromCurrency;
+                    setFromCurrency(toCurrency);
+                    setToCurrency(temp);
+                    let a = fromCountryName;
+                    setFromCountryName(toCountryName);
+                    setToCountryName(a);
+                }} />
             </div>
             <div className="flex-1">
                 <div className="input-heading">To</div>
-                <select name="" id="" className="input-full">
-                    <option value="India"> India</option>
-                    <option value="India"> India</option>
-                    <option value="India"> India</option>
+                <select value={toCurrency} onChange={event=>{setToCurrency(event.target.value); setToCountryName(country[event.target.value])}} className="input-full">
+                    {
+                        Object.keys(country).map((key)=>
+                        (
+                            country[key]? <option value={key}>{country[key]}</option> : null 
+                        ))
+                    }
+                
                 </select>
             </div>
            </div>
            <div>
                 <div className="output-text"> 
-                    1,223.00 US Dollars = 
+                    {inputValue} {fromCountryName} = 
                 </div>
                 <div className="converted-text">
-                    1,671.3685 Canadian Dollars
+                    {inputValue*countryValue} {toCountryName}
                 </div>
            </div>
            <div className="button">
-                 <button className="convert-button">Convert Currency</button>
+                 <button className="convert-button" onClick={handleConvert}>Convert Currency</button>
            </div>
         </div>
-        
-        <div> 
-            {/* {
-            Object.keys(country.object).map((key, i) => (
-                <ul key={i}>
-                    {key}:{country.object[key]}
-                </ul>
-             ))
-         } */}
-        </div>
 
+ 
     </div>)
 }
 
